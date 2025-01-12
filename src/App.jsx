702d413d34d3e5237e942d605807421b0316cf88
h-banerjee <harshbanerjee30@gmail.com>
2025-01-12 17:35:20 +0530

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
-import Food from './Food.jsx'
+import React from 'react';
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
+import Food from './Food.jsx';
+import Card from './Card.jsx';
 
 function App() {
-  return(
+  return (
     <>
-    <Header/>
-    <Food/>
-    <Footer/>
+      
+      <Card>
+        
+      </Card>
+      
     </>
-  )
+  );
 }
 
-export default App
-  
+export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+function Card(){
+    return(
+    <div className="Card">
+        <img></img>
+        <h2>Harsh</h2>
+        <p>I AM DA BEST</p>
+    </div>
+    )
+}
+
+export default Card;
