6c787e468121c519512367232edfe88a0dcbf0f0
h-banerjee <harshbanerjee30@gmail.com>
2025-01-12 13:02:44 +0530

    -m

---
+++ src/Food.jsx
@@ -0,0 +1,13 @@
+function Food(){
+    const food1 = "orange";
+    const food2 = "banana";
+    const food3 = "Apple";
+    return(
+        <ul>
+            <li>Apple</li>
+            <li>Banana</li>
+            <li>Orange</li>
+        </ul>
+    );
+}
+export default Food
